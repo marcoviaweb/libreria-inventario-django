48c8e8615616cb806ff012bb593ed295300ae1e1
Tu nombre <tu.email@ejemplo.com>
2025-07-01 14:38:29 -0400

    Movimientos de Inventario

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ReporteLibrosStockView, ReporteLibrosStockBajoView
+from .views import ReporteLibrosStockView, ReporteLibrosStockBajoView, ReporteMovimientosInventarioView
 
 urlpatterns = [
     path('libros-stock/', ReporteLibrosStockView.as_view(), name='reporte_libros_stock'),
     path('libros-stock-bajo/', ReporteLibrosStockBajoView.as_view(), name='reporte_libros_stock_bajo'),
+    path('movimientos-inventario/', ReporteMovimientosInventarioView.as_view(), name='reporte_movimientos_inventario'),
 ]
--- reportes/views.py
+++ reportes/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.db.models import F
 from libros.models import Libro
+from inventario.models import MovimientoInventario
+from django.utils import timezone
+from django import forms
 
 class ReporteLibrosStockView(ListView):
     model = Libro
@@ -15,5 +18,39 @@ class ReporteLibrosStockBajoView(ListView):
     context_object_name = 'libros'
     def get_queryset(self):
         return Libro.objects.filter(stock_actual__lte=F('stock_minimo')).order_by('titulo')
+
+class FiltroMovimientosForm(forms.Form):
+    tipo = forms.ChoiceField(
+        choices=[('', 'Todos')] + MovimientoInventario.TIPO_MOVIMIENTO_CHOICES,
+        required=False,
+        label='Tipo de Movimiento',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    fecha_inicio = forms.DateField(required=False, label='Desde', widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}))
+    fecha_fin = forms.DateField(required=False, label='Hasta', widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}))
+
+class ReporteMovimientosInventarioView(ListView):
+    model = MovimientoInventario
+    template_name = 'reportes/reporte_movimientos_inventario.html'
+    context_object_name = 'movimientos'
+    paginate_by = 30
+
+    def get_queryset(self):
+        qs = MovimientoInventario.objects.select_related('libro', 'usuario').order_by('-fecha_movimiento')
+        tipo = self.request.GET.get('tipo')
+        fecha_inicio = self.request.GET.get('fecha_inicio')
+        fecha_fin = self.request.GET.get('fecha_fin')
+        if tipo:
+            qs = qs.filter(tipo_movimiento=tipo)
+        if fecha_inicio:
+            qs = qs.filter(fecha_movimiento__date__gte=fecha_inicio)
+        if fecha_fin:
+            qs = qs.filter(fecha_movimiento__date__lte=fecha_fin)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filtro_form'] = FiltroMovimientosForm(self.request.GET)
+        return context
 
 # Create your views here.

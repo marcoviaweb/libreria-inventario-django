7365e39f7a7691419438f3f387338f72c1ac0b43
Tu nombre <tu.email@ejemplo.com>
2025-07-01 14:39:56 -0400

    Reporte de Ventas

--- reportes/urls.py
+++ reportes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ReporteLibrosStockView, ReporteLibrosStockBajoView, ReporteMovimientosInventarioView
+from .views import ReporteLibrosStockView, ReporteLibrosStockBajoView, ReporteMovimientosInventarioView, ReporteVentasView
 
 urlpatterns = [
     path('libros-stock/', ReporteLibrosStockView.as_view(), name='reporte_libros_stock'),
     path('libros-stock-bajo/', ReporteLibrosStockBajoView.as_view(), name='reporte_libros_stock_bajo'),
     path('movimientos-inventario/', ReporteMovimientosInventarioView.as_view(), name='reporte_movimientos_inventario'),
+    path('ventas/', ReporteVentasView.as_view(), name='reporte_ventas'),
 ]
--- reportes/views.py
+++ reportes/views.py
@@ -5,6 +5,7 @@ from libros.models import Libro
 from inventario.models import MovimientoInventario
 from django.utils import timezone
 from django import forms
+from ventas.models import Venta, DetalleVenta
 
 class ReporteLibrosStockView(ListView):
     model = Libro
@@ -51,6 +52,35 @@ class ReporteMovimientosInventarioView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['filtro_form'] = FiltroMovimientosForm(self.request.GET)
+        return context
+
+class FiltroVentasForm(forms.Form):
+    fecha_inicio = forms.DateField(required=False, label='Desde', widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}))
+    fecha_fin = forms.DateField(required=False, label='Hasta', widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}))
+    libro = forms.ModelChoiceField(queryset=Libro.objects.all(), required=False, label='Libro', widget=forms.Select(attrs={'class': 'form-select'}))
+
+class ReporteVentasView(ListView):
+    model = Venta
+    template_name = 'reportes/reporte_ventas.html'
+    context_object_name = 'ventas'
+    paginate_by = 30
+
+    def get_queryset(self):
+        qs = Venta.objects.prefetch_related('detalles__libro', 'usuario').order_by('-fecha_venta')
+        fecha_inicio = self.request.GET.get('fecha_inicio')
+        fecha_fin = self.request.GET.get('fecha_fin')
+        libro_id = self.request.GET.get('libro')
+        if fecha_inicio:
+            qs = qs.filter(fecha_venta__date__gte=fecha_inicio)
+        if fecha_fin:
+            qs = qs.filter(fecha_venta__date__lte=fecha_fin)
+        if libro_id:
+            qs = qs.filter(detalles__libro_id=libro_id).distinct()
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filtro_form'] = FiltroVentasForm(self.request.GET)
         return context
 
 # Create your views here.

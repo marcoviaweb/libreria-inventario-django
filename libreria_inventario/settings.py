6ce0ba8e209d1237ea2131795e0fd68c9b20a040
Tu nombre <tu.email@ejemplo.com>
2025-07-02 21:41:56 -0400

    idioma

--- libreria_inventario/settings.py
+++ libreria_inventario/settings.py
@@ -113,7 +113,7 @@ AUTH_USER_MODEL = 'usuarios.Usuario'
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
